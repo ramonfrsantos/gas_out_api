808f0568ed593515856caa575b5bd8a5182258c5
Ramon Felipe Ribeiro Santos <ramon_r_santos@hotmail.com>
2022-05-01 12:10:30 -0300

    criando projeto

---
+++ gasoutapp/src/main/java/com/gasoutapp/gasoutapp/GasoutappApplication.java
@@ -0,0 +1,13 @@
+package com.gasoutapp.gasoutapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasoutappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasoutappApplication.class, args);
+	}
+
+}
---
+++ gasoutapp/src/test/java/com/gasoutapp/gasoutapp/GasoutappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasoutapp.gasoutapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasoutappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
