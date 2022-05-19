9b2d01c86a91e0951e9fe37fda2698a32f61fe77
Ramon Felipe Ribeiro Santos <ramon_r_santos@hotmail.com>
2022-05-19 09:55:30 -0300

    Update RoomController.java

--- gas-out-app/src/main/java/br/com/gasoutapp/controller/RoomController.java
+++ gas-out-app/src/main/java/br/com/gasoutapp/controller/RoomController.java
@@ -4,19 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.gasoutapp.domain.Room;
 import br.com.gasoutapp.dto.RoomDTO;
 import br.com.gasoutapp.service.RoomService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("room")
